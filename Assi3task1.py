d8efdf1b15a8edf3cb8249a5f1d21818807f5ba2
Ujwal Anand <anandujwal6@gmail.com>
2025-08-27 08:51:03 +0530

    Update Assi3task1.py

--- Assi3task1.py
+++ Assi3task1.py
@@ -1,12 +1,12 @@
-# Function to calculate factorial
 def factorial(n):
-    if n < 0:
-        return "factorial not defined for  negative numbers"
     result = 1
     for i in range(1, n + 1):
         result *= i
-        return result
+    return result
 
-    # Sample call
-    num = 5
-    print("Factorial of", num, "is", factorial(num))
+# Get user input
+num = int(input("Enter a number: "))
+
+# Call the factorial function and print the result
+print(f"Factorial of {num} is: {factorial(num)}")
+
